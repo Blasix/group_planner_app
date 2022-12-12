17db166392709a2f66a635d27e5a8f4c5d723e92
BlasixMac <blasixcorp@gmail.com>
2022-12-12 13:19:00 +0100

    face auth does not work on ios so for the moment i disabled it

--- lib/widgets/auth/meta.dart
+++ lib/widgets/auth/meta.dart
@@ -1,11 +1,11 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:group_planner_app/widgets/auth/other_button.dart';
 
-import '../../consts/firebase_consts.dart';
-import '../../fetch.dart';
+// import '../../consts/firebase_consts.dart';
+// import '../../fetch.dart';
 
 class MetaButton extends StatelessWidget {
   const MetaButton({super.key});
@@ -14,32 +14,32 @@ class MetaButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return OtherButton(
         function: () async {
-          // Trigger the sign-in flow
-          final LoginResult loginResult = await FacebookAuth.instance.login();
+          //   // Trigger the sign-in flow
+          //   final LoginResult loginResult = await FacebookAuth.instance.login();
 
-          // Create a credential from the access token
-          final OAuthCredential facebookAuthCredential =
-              FacebookAuthProvider.credential(loginResult.accessToken!.token);
+          //   // Create a credential from the access token
+          //   final OAuthCredential facebookAuthCredential =
+          //       FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
-          // Once signed in, return the UserCredential
-          await FirebaseAuth.instance
-              .signInWithCredential(facebookAuthCredential);
+          //   // Once signed in, return the UserCredential
+          //   await FirebaseAuth.instance
+          //       .signInWithCredential(facebookAuthCredential);
 
-          final userData = await FacebookAuth.instance.getUserData();
-          final uid = authInstance.currentUser!.uid;
-          await FirebaseFirestore.instance.collection('users').doc(uid).set({
-            'id': uid,
-            'username': userData['name'],
-            'email': userData['email'],
-            'profilePictureUrl': userData['picture']['data']['url'],
-            'createdAt': Timestamp.now(),
-            'selectedTeam': '',
-          });
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(
-              builder: (context) => const Fetch(),
-            ),
-          );
+          //   final userData = await FacebookAuth.instance.getUserData();
+          //   final uid = authInstance.currentUser!.uid;
+          //   await FirebaseFirestore.instance.collection('users').doc(uid).set({
+          //     'id': uid,
+          //     'username': userData['name'],
+          //     'email': userData['email'],
+          //     'profilePictureUrl': userData['picture']['data']['url'],
+          //     'createdAt': Timestamp.now(),
+          //     'selectedTeam': '',
+          //   });
+          //   Navigator.of(context).pushReplacement(
+          //     MaterialPageRoute(
+          //       builder: (context) => const Fetch(),
+          //     ),
+          //   );
         },
         buttonImagePath: 'assets/images/auth/meta.png');
   }
